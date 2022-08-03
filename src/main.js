4d44f66058952ded026dea3b9458d96039df9141
NThaiDat <107167684+NThaiDat@users.noreply.github.com>
2022-08-03 15:12:39 +0700

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
 import App from './App.vue'
 import router from './router'
 
@@ -8,7 +11,7 @@ import './assets/main.css'
 
 
 const app = createApp(App)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
