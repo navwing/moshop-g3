9b8028b5c6a069e635b2fe7f80dd9f4f5f72ead9
NThaiDat <107167684+NThaiDat@users.noreply.github.com>
2022-08-04 15:49:22 +0700

    finish EmployeeManagement

--- src/main.js
+++ src/main.js
@@ -3,15 +3,31 @@ import {createPinia} from 'pinia'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {
-    faAngleDown, faChartSimple, faUserGroup, faComment, faVideo, faUsers, faClipboardList
-    , faArrowUpFromBracket, faCheck, faUserPlus, faUser, faBox, faCircleInfo, faTag, faArrowRightFromBracket
+    faAngleDown,
+    faChartSimple,
+    faUserGroup,
+    faComment,
+    faVideo,
+    faUsers,
+    faClipboardList
+    ,
+    faArrowUpFromBracket,
+    faCheck,
+    faUserPlus,
+    faUser,
+    faBox,
+    faCircleInfo,
+    faTag,
+    faArrowRightFromBracket,
+    faGear,
+    faPlus
 } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
 library.add(faAngleDown, faChartSimple, faUserGroup, faComment, faVideo, faUsers, faClipboardList,
-    faArrowUpFromBracket,faCheck,faUserPlus,faUser,faBox,faCircleInfo,faTag,faArrowRightFromBracket)
+    faArrowUpFromBracket, faCheck, faUserPlus, faUser, faBox, faCircleInfo, faTag, faArrowRightFromBracket, faGear, faPlus)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
--- src/views/EmployeeManagement.vue
+++ src/views/EmployeeManagement.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="container">
-    <div class="row page__header">
+    <div class="row page__header flex">
       <div class="left-content">
         <div class="info flex items-center">
           <h2 class="info-title">Quản lý nhân viên</h2>
           <div class="live">
-            <div class="signal flex items-center justify-center">
+            <div class="signal flex items-center justify-center relative">
               <div class="dot-signal"></div>
+              <div class="circle-signal circle-1 absolute"></div>
+              <div class="circle-signal circle-2 absolute"></div>
             </div>
 
           </div>
+          <div class="pl-4" style="font-style: italic;">
+            Live (cập nhật 14:00)
+          </div>
+        </div>
+        <div class="filter">
+          <button class="filter-btn active">Hôm nay</button>
+          <button class="filter-btn ">Tuần này</button>
+          <button class="filter-btn ">Tháng này</button>
+          <button class="filter-btn ">Tùy chọn</button>
         </div>
       </div>
-      <div class="right-content"></div>
+      <div class="right-content">
+        <div class="action flex items-end flex-wrap  flex-col">
+          <button class="btn-utility">
+            <font-awesome-icon icon="fa-solid fa-gear"/>
+            <span>KPIs nhân viên</span>
+          </button>
+          <a href="#" class="btn-utility">
+            <font-awesome-icon icon="fa-solid fa-plus"/>
+            <span> Thêm nhân viên</span>
+          </a>
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -48,18 +71,83 @@ export default {
   margin-right: 26px;
   margin-bottom: 0.5rem;
 }
-.signal{
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  background-color: #ffc107;
-}
-.dot-signal {
+
+.signal {
   width: 20px;
   height: 20px;
-  background-color: red;
+}
+
+.dot-signal {
+  width: 10px;
+  height: 10px;
+  background-color: #ff5550;
   border-radius: 50%;
-  border: 1px green solid;
-  padding: 10px;
+
+}
+
+.circle-signal {
+  border: 2px #ff5550 solid;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
+
+.circle-1 {
+  animation: signal-animation1 3s infinite;
+}
+
+.circle-2 {
+  animation: signal-animation2 3s infinite;
+}
+
+@keyframes signal-animation1 {
+  from {
+    padding: 5px;
+  }
+  to {
+    padding: 15px;
+  }
+}
+
+@keyframes signal-animation2 {
+  from {
+    padding: 5px;
+  }
+  to {
+    padding: 10px;
+  }
+}
+
+.filter-btn {
+  margin-right: 15px;
+  padding: 4px 20px;
+  margin-bottom: 10px;
+  border-radius: 18px;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  color: #069255;
+  background-color: #efefef
+}
+
+.filter .active {
+  background-color: #069255;
+  color: white;
+}
+
+.btn-utility {
+  margin-bottom: 10px;
+  font-weight: 500;
+  border-radius: 18px;
+  padding: 8px 15px;
+  display: flex;
+  align-items: center;
+  width: 162px;
+  background-color: #069255;
+  color: white;
+  font-size: 14px
+}
+.btn-utility span{
+  margin-left: 10px;
 }
 </style>
