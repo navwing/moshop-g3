d111a7e9c3edc34ed501f17a9f98914a8a7e44ba
NThaiDat <107167684+NThaiDat@users.noreply.github.com>
2022-08-08 15:09:18 +0700

    Merge branch 'main' of D:\study\GHTKVue\moshop-g3 with conflicts.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
+import NavBar from "./components/NavBar.vue";
+import EmployeeManagement from "./views/EmployeeManagement/EmployeeManagement.vue";
 </script>
 
 <template>
+  <NavBar/>
+  <div class="main-content">
+    <div class="page">
+      <EmployeeManagement/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.main-content{
+  overflow: auto;
+  min-height: calc(100% - 48px);
+}
+.page{
+  padding-top: 30px;
+  padding-bottom: 20px;
+}
 </style>
